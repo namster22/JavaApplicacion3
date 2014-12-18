4729d852456b9a5e5cac66ae7880cd1454300ae3
Emilio <Emilio@MSI-GP60>
2014-12-18 20:43:05 +0100

    new commit

---
+++ src/pruebas.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Emilio
+ */
+public class pruebas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     
+        // TODO code application logic here
+    
+    
+    
+    int numMax=100;
+    int numActual=1;
+    int incremento=1;
+    while (numActual<= numMax){
+    System.out.println(numActual);
+    numActual+=incremento;
+    incremento++;
+}
+    
+}
+
+    
+    }
+    
+
